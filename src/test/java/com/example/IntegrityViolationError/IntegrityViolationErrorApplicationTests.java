675983160eeacf2bb75155ec9ecd678b75c254d8
Koushal Kumar <koushalkumar2224@gmail.com>
2024-08-07 20:29:44 +0530

    Deployment

---
+++ src/test/java/com/example/IntegrityViolationError/IntegrityViolationErrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IntegrityViolationError;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrityViolationErrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
