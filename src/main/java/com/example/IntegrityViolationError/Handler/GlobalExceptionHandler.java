93d76d928a7195ea93e640c15d1b6de75702bfbd
Koushal Kumar <koushalkumar2224@gmail.com>
2024-08-07 20:58:04 +0530

    Handler

---
+++ src/main/java/com/example/IntegrityViolationError/Handler/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.example.IntegrityViolationError.Handler;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationException(MethodArgumentNotValidException exception){
+        Map<String,String> errors=new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach(error ->
+        {
+            String fieldName=((FieldError)error).getField();
+            String errorMessage=error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<?> handleUniqueDataEntry(DataIntegrityViolationException exception, WebRequest request){
+        String errorMessage="Phone Number Already Registered";
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+}
